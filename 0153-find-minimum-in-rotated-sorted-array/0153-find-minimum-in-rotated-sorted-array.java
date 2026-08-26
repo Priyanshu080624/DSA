18571069a7885e508b326c63a597ae95277f13f2
Priyanshu kulhari <priyanshu080624@gmail.com>
2026-08-26 19:19:01 +0530

    Time: 0 ms (100%), Space: 44 MB (20.35%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -3,15 +3,13 @@ class Solution {
         int low =0;
         int high = nums.length-1;
         if(nums.length==1) return nums[0];
-        int r = low + (high-low)/2;
-        //System.out.print(nums[r]);
-        if(nums.length>3&&nums[r]<nums[r-1]&&nums[r]<nums[r+1]) return nums[r];
+        //int r = low + (high-low)/2;
+        //if(nums.length>3&&nums[r]<nums[r-1]&&nums[r]<nums[r+1]) return nums[r];
         while(low<=high){
             int mid = low + (high-low)/2;
-            //System.out.print(nums[mid]+"\n");
-            if(mid-1>=0&&mid+1<nums.length&&nums.length>3&&nums[mid]<nums[mid-1]&&nums[mid]<nums[mid+1]) return nums[mid];
+            //if(mid-1>=0&&mid+1<nums.length&&nums.length>3&&nums[mid]<nums[mid-1]&&nums[mid]<nums[mid+1]) return nums[mid];
             if(nums[low]>nums[high]&&nums[mid]<nums[high]||nums[low]<nums[high]&&nums[mid]<nums[high]){
-                high = mid-1;
+                high = mid;
             }else low = mid+1;
         }
         if(high<0) high =0;
