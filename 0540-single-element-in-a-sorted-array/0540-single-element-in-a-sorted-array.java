d553523d9b32a841708137a0a862dfe328294913
Priyanshu kulhari <priyanshu080624@gmail.com>
2026-08-26 21:35:12 +0530

    Time: 0 ms (100%), Space: 52.7 MB (90.56%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -4,12 +4,8 @@ class Solution {
         int high = nums.length -1;
         while(low<high){
             int mid = low + (high - low)/2;
-            if(mid ==0){
-                return nums[mid];
-            }
-            if(mid==nums.length-1){
-                return nums[nums.length-1];
-            }
+            if(mid ==0) return nums[mid];
+            if(mid==nums.length-1) return nums[nums.length-1];            
             if(nums[mid+1]==nums[mid]){
                 if(mid%2==0){
                     low = mid +1;
